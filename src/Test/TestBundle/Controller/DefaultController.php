79ceb5d98ba88d909b1b1a1368e29d1e56399248
pmoroz <pmoroz@malkosua.com>
2015-04-27 18:41:39 +0300

    Add ext js

--- src/Test/TestBundle/Controller/DefaultController.php
+++ src/Test/TestBundle/Controller/DefaultController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction($name = "Dev")
     {
         return $this->render('TestTestBundle:Default:index.html.twig', array('name' => $name));
     }
